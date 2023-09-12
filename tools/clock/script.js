a1948d86345368de820680cb5ede218d731b3bce
c0nstantly <116290617+c0nstantly@users.noreply.github.com>
2023-09-12 10:56:50 -0400

    Update script.js

--- tools/clock/script.js
+++ tools/clock/script.js
@@ -37,6 +37,7 @@ function Time() {
 
  // Adding time elements to the div
  document.getElementById("digital-clock").innerText = hour + ":" + minute + ":" + second + " " + period;
+ document.getElementById("digital-clock-title").innerText = hour + ":" + minute + ":" + second + " " + period;
 
  // Set Timer to 1 sec (1000 ms)
  setTimeout(Time, 1000);
